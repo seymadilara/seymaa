03416aaeae6f28070b55183f20d3139974577dc9
ŞEYMA <dpehlivan086@gmail.com>
2025-11-03 15:36:13 +0300

    Refactor seyma.py for improved readability

--- seyma.py
+++ seyma.py
@@ -1,7 +1,6 @@
 #1. Kullanıcıdan adını ve soyadını ayrı ayrı alıp, tam adını ekrana yazdıran program.
 ad = input("Adınızı girin: ")
 soyad = input("Soyadınızı girin: ")
-tam_ad = ad + " " + soyad
 print("Tam adınız:", tam_ad)
 
 #2.İki sayının toplamı, farkı ve çarpımı
@@ -13,19 +12,19 @@ print(x * y)
 
 #3.Kullanıcının yaşını alıp, 18 yaşından büyük olup olmadığını True/False olarak gösteren program
 yas = input("lütfen yaşınızı girin")
-sonuç = int(yas)<18
+sonuç = int(yas)<19
 print(sonuç)
 
 #4.Dikdörtgenin alanını ve çevresini hesaplayan program
-kisa_kenar = float(input("Kısa kenarı girin: "))
-uzun_kenar = float(input("Uzun kenarı girin: "))
-alan = kisa_kenar * uzun_kenar
-cevre = 2 * (kisa_kenar + uzun_kenar)
+kisa kenar = int(input("Kısa kenarı girin: "))
+uzun kenar = int(input("Uzun kenarı girin: "))
+alan = kisa_kenar * uzun kenar
+cevre = 2 * (kisa kenar + uzun kenar)
 print("Alan:", alan)
 print("Çevre:", cevre)
 
 #5. Girilen bir sayının pozitif olup olmadığını kontrol eden program.
-sayi = float(input("Bir sayı girin: "))
+sayi = int(input("Bir sayı girin: "))
 pozitif_mi = 10 > 0
 print("Sayı pozitif mi?", pozitif_mi)
 
@@ -46,7 +45,7 @@ print(ortalama)
 #8.Kullanıcının girdiği iki sayının her ikisinin de çift sayı olup olmadığını kontrol eden program
 sayi1 = int(input("1. sayıyı girin: "))
 sayi2 = int(input("2. sayıyı girin: "))
-sonuç = (sayi1 % 2 == 0) and (sayi2 % 2 == 0)
+if sayi1 % 2 == 0 or sayi2 % 2 == 0
 print("Her iki sayı da çift mi?", sonuç)
 
 #9.Girilen bir metnin uzunluğunu ve büyük harfe çevrilmiş halini ekrana yazdıran program
@@ -58,7 +57,7 @@ print("Büyük harf hali:", buyuk_harf)
 
 #10.Dairenin alanını hesaplayan program (pi = 3.14)
 pi = 3.14
-yaricap = float(input("Dairenin yarıçapını girin: "))
+yaricap = int(input("Dairenin yarıçapını girin: "))
 alan = pi * (yaricap ** 2)
 print("Dairenin alanı:", alan)
 
@@ -72,6 +71,7 @@ print("Birinci sayı ikinciden büyük mü?", birinci_buyuk_mu)
 
 #12.Bir sayının hem 3’e hem de 5’e tam bölünüp bölünmediğini kontrol eden program
 sayi = int(input("Bir sayı girin: "))
-bolunur_mu = (sayi % 3 == 0) and (sayi % 5 == 0)
-print("Sayı hem 3'e hem de 5'e tam bölünüyor mu?", bolunur_mu)
+a = (sayi % 3 == 0) and (sayi % 5 == 0)
+print("Sayı hem 3'e hem de 5'e tam bölünüyor mu?", a)
+
 
